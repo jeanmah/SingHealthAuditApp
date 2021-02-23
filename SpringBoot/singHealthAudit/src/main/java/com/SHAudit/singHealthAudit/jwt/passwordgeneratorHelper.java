79944ac907d65cc1bce2c41c4f9cbfc3b6045386
jeanmah <jeanmah.828@gmail.com>
2021-02-23 23:38:53 +0800

    tryyyy

--- SpringBoot/singHealthAudit/src/main/java/com/SHAudit/singHealthAudit/jwt/passwordgeneratorHelper.java
+++ SpringBoot/singHealthAudit/src/main/java/com/SHAudit/singHealthAudit/jwt/passwordgeneratorHelper.java
@@ -5,8 +5,9 @@ public class passwordgeneratorHelper {
 
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "testing12ddf3";
-        String encodedPassword = encoder.encode(rawPassword);
+        String rawPasrd = "testing12ddfgfdsg3";
+        int t = 234;
+        String encodedPassword = encoder.encode(rawPasrd);
 
         System.out.println(encodedPassword); //$2a$10$4h6b0xhD2A64FgQ8B2CeaOSLI3ZpLexh5fyEaEPt9tyI33V5pkdim
     }
