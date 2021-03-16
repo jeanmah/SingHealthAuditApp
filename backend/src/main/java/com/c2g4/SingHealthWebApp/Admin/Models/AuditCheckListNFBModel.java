5f1dde057eebe27959742d93bc205927fd8d0d10
jeanmah <jeanmah.828@gmail.com>
2021-03-17 01:01:25 +0800

    changed (n)fbchecklist

--- backend/src/main/java/com/c2g4/SingHealthWebApp/Admin/Models/AuditCheckListNFBModel.java
+++ backend/src/main/java/com/c2g4/SingHealthWebApp/Admin/Models/AuditCheckListNFBModel.java
@@ -8,6 +8,7 @@ public class AuditCheckListNFBModel {
     @Id
     private int nfb_qn_id;
     private String category;
+    private String sub_category;
     private double weight;
     private String requirement;
     private String sub_requirement;
@@ -22,6 +23,13 @@ public class AuditCheckListNFBModel {
 	}
 	public void setCategory(String category) {
 		this.category = category;
+	}
+	public String getSub_category() {
+		return sub_category;
+	}
+
+	public void setSub_category(String sub_category) {
+		this.sub_category = sub_category;
 	}
 	public double getWeight() {
 		return weight;
