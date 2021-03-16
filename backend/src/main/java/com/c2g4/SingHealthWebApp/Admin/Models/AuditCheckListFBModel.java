5672ecaebd6587457a47faefa724a057cbf70cf7
BuildTools <unconfigured@null.spigotmc.org>
2021-03-17 01:52:05 +0800

    Some minor changes by Hannah

--- backend/src/main/java/com/c2g4/SingHealthWebApp/Admin/Models/AuditCheckListFBModel.java
+++ backend/src/main/java/com/c2g4/SingHealthWebApp/Admin/Models/AuditCheckListFBModel.java
@@ -8,7 +8,7 @@ public class AuditCheckListFBModel {
     @Id
     private int fb_qn_id;
     private String category;
-	private String sub_category;
+    private String sub_category;
     private double weight;
     private String requirement;
     private String sub_requirement;
@@ -24,13 +24,6 @@ public class AuditCheckListFBModel {
 	}
 	public void setCategory(String category) {
 		this.category = category;
-	}
-	public String getSub_category() {
-		return sub_category;
-	}
-
-	public void setSub_category(String sub_category) {
-		this.sub_category = sub_category;
 	}
 	public double getWeight() {
 		return weight;
@@ -50,6 +43,12 @@ public class AuditCheckListFBModel {
 	public void setSub_requirement(String sub_requirement) {
 		this.sub_requirement = sub_requirement;
 	}
-
+	public String getSub_category() {
+		return sub_category;
+	}
+	public void setSub_category(String sub_category) {
+		this.sub_category = sub_category;
+	}
+    
     
 }
--- backend/src/main/java/com/c2g4/SingHealthWebApp/Admin/Models/AuditCheckListNFBModel.java
+++ backend/src/main/java/com/c2g4/SingHealthWebApp/Admin/Models/AuditCheckListNFBModel.java
@@ -24,13 +24,6 @@ public class AuditCheckListNFBModel {
 	public void setCategory(String category) {
 		this.category = category;
 	}
-	public String getSub_category() {
-		return sub_category;
-	}
-
-	public void setSub_category(String sub_category) {
-		this.sub_category = sub_category;
-	}
 	public double getWeight() {
 		return weight;
 	}
@@ -48,6 +41,12 @@ public class AuditCheckListNFBModel {
 	}
 	public void setSub_requirement(String sub_requirement) {
 		this.sub_requirement = sub_requirement;
+	}
+	public String getSub_category() {
+		return sub_category;
+	}
+	public void setSub_category(String sub_category) {
+		this.sub_category = sub_category;
 	}
     
     
